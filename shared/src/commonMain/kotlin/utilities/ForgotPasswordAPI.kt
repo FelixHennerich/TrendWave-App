0cec85c9c5eb6cd5ce1d91881fa3ec608a2a084b
Felix Hennerich <66116721+BlueNitrox@users.noreply.github.com>
2023-10-06 18:29:39 +0200

    continued passwordapi

--- shared/src/commonMain/kotlin/utilities/ForgotPasswordAPI.kt
+++ shared/src/commonMain/kotlin/utilities/ForgotPasswordAPI.kt
@@ -1,15 +1,17 @@
 package utilities
 
 import io.ktor.client.HttpClient
+import io.ktor.client.request.get
 
 class ForgotPasswordAPI {
 
     private val client = HttpClient()
     private val url = "http://85.215.41.146/php/RESTfulAPI/"
 
-    fun getAuthcode() {
+    suspend fun getAuthcode() {
         val finurl = url + "emailAuthGetter.php"
 
+        val response = client.get(finurl)
     }
 
     fun getAuthCodeByUser(uuid: String): String?{
