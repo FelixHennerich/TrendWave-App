695b3ecdade2db27469573abe3477effd74f728c
Felix Hennerich <66116721+BlueNitrox@users.noreply.github.com>
2023-11-29 20:44:47 +0100

    Fixed #164

--- shared/src/commonMain/kotlin/managers/DataStorageOnLogin.kt
+++ shared/src/commonMain/kotlin/managers/DataStorageOnLogin.kt
@@ -1,10 +1,14 @@
 package managers
 
+import utilities.CommonLogger
+
 class DataStorageOnLogin(
     private val localDataManager: DataStorageManager
 ) {
 
     fun storeData(user: String, password: String, username: String, role: String, uuid: String){
+        val commonLogger = CommonLogger()
+        commonLogger.log(role)
         localDataManager.saveString("email", user)
         localDataManager.saveString("password", password)
         localDataManager.saveString("username", username)
