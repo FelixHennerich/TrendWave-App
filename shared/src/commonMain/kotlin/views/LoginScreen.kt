ca880d1c3f427dd3c89f5852035c7d55613638b4
Felix Hennerich <66116721+BlueNitrox@users.noreply.github.com>
2023-12-17 15:35:12 +0100

    login redesign

--- shared/src/commonMain/kotlin/views/LoginScreen.kt
+++ shared/src/commonMain/kotlin/views/LoginScreen.kt
@@ -27,7 +27,6 @@ import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -39,11 +38,11 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import event.TrendWaveEvent
@@ -55,6 +54,8 @@ import managers.DataStorageOnLogin
 import managers.exceptions.ExceptionHandler
 import managers.exceptions.NException
 import org.jetbrains.compose.resources.ExperimentalResourceApi
+import utilities.color.Colors
+import utilities.color.fromEnum
 import utilities.presentation.SideSheet
 import views.sheet.ForgotPasswordSheet
 
@@ -79,18 +80,20 @@ class LoginScreen {
         onNavigateRegister: () -> Unit,
         onNavigateHome: () -> Unit,
         imageDataSource: ImageDataSource,
-        localDataManager: DataStorageManager
+        localDataManager: DataStorageManager,
     ) {
         var user by remember { mutableStateOf("") }
         var password by remember { mutableStateOf("") }
         var passwordVisible by rememberSaveable { mutableStateOf(false) }
+        val cornerrad = 10.dp
+        var corner = RoundedCornerShape(cornerrad)
 
 
         Column(
-            Modifier
+            modifier = Modifier
                 .fillMaxHeight()
                 .fillMaxWidth()
-                .background(color = Color.White),
+                .background(color = Color.fromEnum(Colors.PRIMARY)),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             // Logo of the APP
@@ -98,6 +101,7 @@ class LoginScreen {
             var loading by remember { mutableStateOf(true) }
 
 
+            //Logo
             if (loading) {
                 LaunchedEffect(loading) {
                     imageBytes = imageDataSource.getImage("LogoTransparent.jpg")
@@ -105,6 +109,7 @@ class LoginScreen {
                 }
             }
 
+            //Logo
             imageBytes?.let {
                 Photo(
                     width = 250.dp,
@@ -115,14 +120,19 @@ class LoginScreen {
             // -----------------
 
 
+            //Space
             Spacer(modifier = Modifier.height(40.dp))
+
+            //Login Headline
             Text(
-                "LOGIN",
+                text = "LOGIN",
                 fontSize = 30.sp,
                 fontStyle = FontStyle.Italic,
                 fontWeight = FontWeight.Bold,
-                color = Color.Blue
+                color = Color.fromEnum(Colors.SENARY)
             )
+
+
             // Textfield for the E-mail / Username
             TextField(
                 value = user,
@@ -131,7 +141,9 @@ class LoginScreen {
                         contentAlignment = Alignment.Center
                     ) {
                         Text(
-                            text = "E-mail"
+                            text = "E-mail",
+                            modifier = Modifier.offset(y = -(2).dp, x = 3.dp),
+                            color = Color.fromEnum(Colors.SENARY)
                         )
                     }
                 },
@@ -141,42 +153,22 @@ class LoginScreen {
                     keyboardType = KeyboardType.Email,
                     imeAction = ImeAction.Done,
                 ),
-                keyboardActions = KeyboardActions(
-                    onDone = {
-                        GlobalScope.launch {
-                            val loginManager = LoginManager()
-                            val exceptionHandler = ExceptionHandler()
-                            val userClass = AppUser()
-                            val message = exceptionHandler.fetchErrorMessage(
-                                loginManager.login(
-                                    email = user,
-                                    password = password
-                                )
-                            )
-
-                            onEvent(TrendWaveEvent.ChangeLoginErrorMessage(message))
-                            if (message == exceptionHandler.fetchErrorMessage(NException.SUCCESS001)) {
-                                val uuid = userClass.getUUID(user)
-                                val username = userClass.getUsername(uuid)
-                                val role = userClass.getRole(uuid)
-
-                                val DataStorageOnLogin = DataStorageOnLogin(localDataManager)
-                                DataStorageOnLogin.storeData(user, password, username, role, uuid)
-
-                                onNavigateHome()
-                            }
-                        }
-                    }
-                ),
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(66.dp)
                     .padding(start = 64.dp, end = 64.dp, top = 8.dp, bottom = 8.dp)
-                    .border(1.dp, color = Color.Blue, shape = RoundedCornerShape(50)),
-                shape = RoundedCornerShape(50),
+                    .background(
+                        color = Color.fromEnum(Colors.TERTIARY),
+                        shape = corner
+                    )
+                    .border(
+                        width = 1.dp,
+                        color = Color.fromEnum(Colors.SENARY),
+                        shape = corner
+                    ),
                 textStyle = TextStyle(
                     textAlign = TextAlign.Left,
-                    color = Color.Blue,
+                    color = Color.fromEnum(Colors.SENARY),
                     fontSize = 14.sp
                 ),
                 colors = TextFieldDefaults.textFieldColors(
@@ -184,7 +176,15 @@ class LoginScreen {
                     focusedIndicatorColor = Color.Transparent,
                     unfocusedIndicatorColor = Color.Transparent
                 ),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        onDone(user, password, localDataManager, onEvent, onNavigateHome)
+                    }
+                )
             )
+
+
+
             // Textfield for the password
             TextField(
                 value = password,
@@ -193,20 +193,35 @@ class LoginScreen {
                         contentAlignment = Alignment.Center
                     ) {
                         Text(
-                            text = "Password"
+                            text = "Password",
+                            modifier = Modifier.offset(y = -(3).dp, x = 3.dp),
+                            color = Color.fromEnum(Colors.SENARY)
                         )
                     }
                 },
                 onValueChange = { text -> password = text },
+                visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(
+                    autoCorrect = false,
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Done,
+                ),
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(66.dp)
                     .padding(start = 64.dp, end = 64.dp, top = 8.dp, bottom = 8.dp)
-                    .border(1.dp, color = Color.Blue, shape = RoundedCornerShape(50)),
-                shape = RoundedCornerShape(50),
+                    .background(
+                        color = Color.fromEnum(Colors.TERTIARY),
+                        shape = corner
+                    )
+                    .border(
+                        width = 1.dp,
+                        color = Color.fromEnum(Colors.SENARY),
+                        shape = corner
+                    ),
                 textStyle = TextStyle(
                     textAlign = TextAlign.Left,
-                    color = Color.Blue,
+                    color = Color.fromEnum(Colors.SENARY),
                     fontSize = 14.sp
                 ),
                 colors = TextFieldDefaults.textFieldColors(
@@ -214,84 +229,40 @@ class LoginScreen {
                     focusedIndicatorColor = Color.Transparent,
                     unfocusedIndicatorColor = Color.Transparent
                 ),
-                visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(
-                    autoCorrect = false,
-                    keyboardType = KeyboardType.Password,
-                    imeAction = ImeAction.Done,
-                ),
                 keyboardActions = KeyboardActions(
                     onDone = {
-                        GlobalScope.launch {
-                            val loginManager = LoginManager()
-                            val exceptionHandler = ExceptionHandler()
-                            val userClass = AppUser()
-                            val message = exceptionHandler.fetchErrorMessage(
-                                loginManager.login(
-                                    email = user,
-                                    password = password
-                                )
-                            )
-
-                            onEvent(TrendWaveEvent.ChangeLoginErrorMessage(message))
-                            if (message == exceptionHandler.fetchErrorMessage(NException.SUCCESS001)) {
-                                val uuid = userClass.getUUID(user)
-                                val username = userClass.getUsername(uuid)
-                                val role = userClass.getRole(uuid)
-
-                                val DataStorageOnLogin = DataStorageOnLogin(localDataManager)
-                                DataStorageOnLogin.storeData(user, password, username, role, uuid)
-
-                                onNavigateHome()
-                            }
-                        }
+                        onDone(user, password, localDataManager, onEvent, onNavigateHome)
                     }
                 )
             )
+
+
+
+            //Error Msg
             Text(text = state.LoginErrorMessage ?: "", color = Color.Red)
+
+            //Forgot password button
             TextButton(onClick = {
                 onEvent(TrendWaveEvent.ClickForgotPasswordSheet)
             }) {
-                Text(text = "Forgot your password?", color = Color.Blue)
+                Text(text = "Forgot your password?", color = Color.fromEnum(Colors.SENARY))
             }
 
             // Button and other UI elements
             Button(
                 onClick = {
-                    GlobalScope.launch {
-                        val loginManager = LoginManager()
-                        val exceptionHandler = ExceptionHandler()
-                        val userClass = AppUser()
-                        val message = exceptionHandler.fetchErrorMessage(
-                            loginManager.login(
-                                email = user,
-                                password = password
-                            )
-                        )
-
-                        onEvent(TrendWaveEvent.ChangeLoginErrorMessage(message))
-                        if (message == exceptionHandler.fetchErrorMessage(NException.SUCCESS001)) {
-                            val uuid = userClass.getUUID(user)
-                            val username = userClass.getUsername(uuid)
-                            val role = userClass.getRole(uuid)
-
-                            val DataStorageOnLogin = DataStorageOnLogin(localDataManager)
-                            DataStorageOnLogin.storeData(user, password, username, role, uuid)
-
-                            onNavigateHome()
-                        }
-                    }
+                    onDone(user, password, localDataManager, onEvent, onNavigateHome)
                 },
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(66.dp)
                     .padding(start = 65.dp, end = 64.dp, top = 8.dp, bottom = 8.dp),
-                colors = ButtonDefaults.buttonColors(backgroundColor = Color.Blue),
-                shape = RoundedCornerShape(50)
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color.fromEnum(Colors.QUATERNARY)),
+                shape = corner
             ) {
                 Text(
                     text = "Login",
-                    color = Color.White,
+                    color = Color.fromEnum(Colors.SENARY),
                     fontSize = 18.sp,
                     fontWeight = FontWeight.Bold
                 )
@@ -302,21 +273,50 @@ class LoginScreen {
             }) {
                 Text(
                     text = "Don't have an Account yet?",
-                    Modifier.padding(top = 8.dp, bottom = 8.dp),
+                    modifier = Modifier.padding(top = 8.dp, bottom = 8.dp),
                     fontSize = 14.sp,
-                    color = Color.Blue
+                    color = Color.fromEnum(Colors.SENARY)
                 )
             }
         }
         SideSheet(
             visible = state.isForgetPasswordSheetOpen,
             modifier = Modifier.fillMaxSize(),
-            backgroundcolor = Color.White,
+            backgroundcolor = Color.fromEnum(Colors.PRIMARY),
         ){
             ForgotPasswordSheet(
                 onEvent = onEvent,
                 state = state
             )
+        }
+    }
+
+
+    fun onDone(email: String, password: String, localDataManager: DataStorageManager, onEvent: (TrendWaveEvent) -> Unit, onNavigateHome: () -> Unit){
+        if(email != null && password != null) {
+            GlobalScope.launch {
+                val loginManager = LoginManager()
+                val exceptionHandler = ExceptionHandler()
+                val userClass = AppUser()
+                val message = exceptionHandler.fetchErrorMessage(
+                    loginManager.login(
+                        email = email,
+                        password = password
+                    )
+                )
+
+                onEvent(TrendWaveEvent.ChangeLoginErrorMessage(message))
+                if (message == exceptionHandler.fetchErrorMessage(NException.SUCCESS001)) {
+                    val uuid = userClass.getUUID(email)
+                    val username = userClass.getUsername(uuid)
+                    val role = userClass.getRole(uuid)
+
+                    val DataStorageOnLogin = DataStorageOnLogin(localDataManager)
+                    DataStorageOnLogin.storeData(email, password, username, role, uuid)
+
+                    onNavigateHome()
+                }
+            }
         }
     }
 }
